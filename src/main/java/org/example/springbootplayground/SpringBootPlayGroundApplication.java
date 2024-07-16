9c9144444d5e5b176930ee84cdf00dc59b3e5c0f
gilbert9172 <gilbert9172@naver.com>
2024-07-16 23:45:58 +0900

    [first commit] spring boot 놀이터 프로젝트

---
+++ src/main/java/org/example/springbootplayground/SpringBootPlayGroundApplication.java
@@ -0,0 +1,13 @@
+package org.example.springbootplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPlayGroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPlayGroundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/springbootplayground/SpringBootPlayGroundApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springbootplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPlayGroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
