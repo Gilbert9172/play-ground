51e781b8371c3487e0e56c1c279ad06994000a58
gilbert9172 <gilbert9172@naver.com>
2024-07-20 01:02:01 +0900

    [feature-001] #1 : multi module 생성
    * issue : JPA ddl-auto 설정이 정상 동작하지 않음.

---
+++ internal-client-api/src/test/java/org/client/api/InternalClientApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.client.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternalClientApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
